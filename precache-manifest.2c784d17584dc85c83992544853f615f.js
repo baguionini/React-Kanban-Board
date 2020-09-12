344f9f314e15e5659ad6cde64823be023bec71f0
Vincent Alec F. Baguio <vfbaguio@up.edu.ph>
2020-09-13 02:54:57 +0800

    Updates

---
+++ precache-manifest.2c784d17584dc85c83992544853f615f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e45a943853ebb1468105b84c264535f",
+    "url": "/React-Kanban-Board/index.html"
+  },
+  {
+    "revision": "3a230e0b4bd8474c2473",
+    "url": "/React-Kanban-Board/static/css/main.96b79e10.chunk.css"
+  },
+  {
+    "revision": "12928e30947946addfb0",
+    "url": "/React-Kanban-Board/static/js/2.ac86e937.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Kanban-Board/static/js/2.ac86e937.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a230e0b4bd8474c2473",
+    "url": "/React-Kanban-Board/static/js/main.e77d5f1f.chunk.js"
+  },
+  {
+    "revision": "cd15566372daf78763ca",
+    "url": "/React-Kanban-Board/static/js/runtime-main.4236f5c7.js"
+  }
+]);
